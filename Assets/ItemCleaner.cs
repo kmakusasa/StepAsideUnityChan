21b7a3158f2c7baf8658bceab598357bd897d3ca
kmakusasa <s-kaito17@outlook.jp>
2020-05-25 17:31:52 +0900

    StepAsideUnityChan

---
+++ Assets/ItemCleaner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+class ItemCleaner : MonoBehaviour
+{
+
+    void Updete()
+    {
+        if (this.transform.position.z < Camera.main.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
